95c099abe49aa6ea31e93c8559b289e7af8c97b9
ndinh215 <blackjackptit@yahoo.com>
2020-07-18 11:01:02 +0700

    Add testing to td4.

--- AlbumRestService/src/test/java/net/iei/rest/service/AlbumResourceTest.java
+++ AlbumRestService/src/test/java/net/iei/rest/service/AlbumResourceTest.java
@@ -10,12 +10,34 @@ import static org.hamcrest.CoreMatchers.is;
 public class AlbumResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testGetAlbumsEndpoint() {
         given()
-          .when().get("/rest")
+          .when().get("/rest/sampleservice/albums")
           .then()
-             .statusCode(200)
-             .body(is("hello"));
+             .statusCode(200);
     }
 
+    @Test
+    public void testGetAlbumEndpoint() {
+        given()
+                .when().get("/rest/sampleservice/album/1")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testGetJSONAlbumsEndpoint() {
+        given()
+                .when().get("/rest/sampleservice/json/albums")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testGetJSONAlbumEndpoint() {
+        given()
+                .when().get("/rest/sampleservice/json/album/1")
+                .then()
+                .statusCode(200);
+    }
 }
